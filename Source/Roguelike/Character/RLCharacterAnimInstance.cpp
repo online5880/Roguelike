8ad29ed9c5f4f1db6c2ff513513142ddf240d69e
Online5880 <gjtjqkr1@naver.com>
2022-05-07 03:10:53 +0900

    2022/05/07 Lean

--- Source/Roguelike/Character/RLCharacterAnimInstance.cpp
+++ Source/Roguelike/Character/RLCharacterAnimInstance.cpp
@@ -2,4 +2,47 @@
 
 
 #include "RLCharacterAnimInstance.h"
+#include "RLCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
+void URLCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Character = Cast<ARLCharacter>(TryGetPawnOwner());
+}
+
+void URLCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if(Character == nullptr)
+	{
+		Character = Cast<ARLCharacter>(TryGetPawnOwner());
+	}
+	if(Character == nullptr) {return;}
+	
+	FVector Velocity = Character->GetVelocity();
+	Velocity.Z = 0;
+	Speed  = Velocity.Size();
+	
+	bIsInAir = Character->GetCharacterMovement()->IsFalling();
+	bIsAccelerating = Character->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+
+	Lean(DeltaSeconds);
+}
+
+void URLCharacterAnimInstance::Lean(float DeltaTime)
+{
+	if(Character == nullptr) return;
+	
+	CharacterRotationLastFrame = CharacterRotation;
+	CharacterRotation = Character->GetActorRotation();
+	
+	const FRotator Delta(UKismetMathLibrary::NormalizedDeltaRotator(CharacterRotation, CharacterRotationLastFrame));
+
+	const float Target = (Delta.Yaw / DeltaTime);
+	const float Interp(FMath::FInterpTo(YawDelta, Target, DeltaTime, 6.f));
+	YawDelta = FMath::Clamp(Interp, -90.f, 90.f);
+}
--- Source/Roguelike/Character/RLCharacterAnimInstance.h
+++ Source/Roguelike/Character/RLCharacterAnimInstance.h
@@ -13,5 +13,37 @@ UCLASS()
 class ROGUELIKE_API URLCharacterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	// *  캐릭터 기울기
+	void Lean(float DeltaTime);
+
+private:
+
+	// * Character
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	class ARLCharacter* Character;
+
+	// * 속도
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	// * 공중에 있는지 체크
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
 	
+	// * 움직이는 중인지 체크
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+
+	// * Yaw Value
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float YawDelta;
+
+	// ? 캐릭터 회전값 (현재,마지막)
+	FRotator CharacterRotation;
+	FRotator CharacterRotationLastFrame;
 };
